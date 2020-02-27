956a3d42ebd64d480523626683ddab3e27dffcf0
FabiusE <fabius.engel@msg.group>
2020-02-27 20:15:01 +0100

    Nginx and Jenkins now deploy on Azure Kubernetes Cluster

--- main.tf
+++ main.tf
@@ -7,6 +7,20 @@ provider "azurerm" {
     tenant_id       = var.tenant_id
 }
 
+// provider "google" {
+//     credentials = file("testproject-ef39e40d4d53.json")
+//     region  = "europe-west3"
+//     zone    = "europe-west3-a"
+// }
+
+// module "gke" {
+//     source = "git::git@gitlab.hrz.tu-chemnitz.de:faeng--tu-chemnitz.de/terraform-google-kubernetes.git"
+
+//     gke_cluster_name    = "examplecluster"
+//     location            = "europe-west3"
+//     project             = "testproject-268907"
+// }
+
 module "aks" {
     source          = "git::git@gitlab.hrz.tu-chemnitz.de:faeng--tu-chemnitz.de/terraform-azurerm-azurekubernetes.git"
 
@@ -18,10 +32,41 @@ module "aks" {
     dns_prefix          = "exampledns"
 }
 
-module "acr" {
-    source = "git::git@gitlab.hrz.tu-chemnitz.de:faeng--tu-chemnitz.de/terraform_azurerm_azurecontaierregistry.git"
+// module "acr" {
+//     source = "git::git@gitlab.hrz.tu-chemnitz.de:faeng--tu-chemnitz.de/terraform_azurerm_azurecontaierregistry.git"
 
-    name                = "fabiusacrformaster"
-    resource_group_name = "examplergforacr"
-    location            = "West Europe"
+//     name                = "fabiusacrformaster"
+//     resource_group_name = "examplergforacr"
+//     location            = "West Europe"
+// }
+
+provider "kubernetes" {
+  version                = "1.10.0"
+
+  load_config_file = "false"
+
+  host                   = module.aks.host
+  client_certificate     = base64decode(module.aks.client_certificate)
+  client_key             = base64decode(module.aks.client_key)
+  cluster_ca_certificate = base64decode(module.aks.cluster_ca_certificate)
+}
+
+provider "helm" {
+    version = "~>1.0.0"
+
+    kubernetes {
+      host                   = module.aks.host
+      client_certificate     = base64decode(module.aks.client_certificate)
+      client_key             = base64decode(module.aks.client_key)
+      cluster_ca_certificate = base64decode(module.aks.cluster_ca_certificate)
+      load_config_file       = false
+  }
+}
+
+module "nginx" {
+    source  = "git::https://gitlab.hrz.tu-chemnitz.de/faeng--tu-chemnitz.de/terraform_nginx_helm.git"
+}
+
+module "jenkins" {
+  source  = "git::https://gitlab.hrz.tu-chemnitz.de/faeng--tu-chemnitz.de/jenkins_terraform_module.git"
 }
